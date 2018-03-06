374c2608d8010c9f557ac505ac8e7e9af408fc6a
Jon Parsons <jon@ctrlxctrlv.co.uk>
2018-03-06 11:34:40 +0000

    refactor: move test stubs into tests folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
-var path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: path.resolve(process.cwd(), "src/index.js"),
-    runner: path.resolve(process.cwd(), "src/runner.js"),
+    index: path.resolve(process.cwd(), 'src/index.js'),
+    runner: path.resolve(process.cwd(), 'tests/index.js'),
   },
   output: {
-    path: path.resolve(process.cwd(), "dist"),
-    filename: "[name].js"
+    path: path.resolve(process.cwd(), 'dist'),
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         test: /.js$/,
-        include: path.resolve(process.cwd(), "src"),
+        include: path.resolve(process.cwd(), 'src'),
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
-  plugins: [
-    new UglifyJsPlugin()
-  ]
+  plugins: [new UglifyJsPlugin()],
 };
